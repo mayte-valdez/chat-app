d9f7cbd06e93539ac2b710900df13262067e5fcb
Mayte <mayte.valdez.abad@gmail.com>
2021-01-03 20:12:21 +0100

    quete React 4 - map list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import Contact from './components/Contact'
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        name='Naomi Knight'
-        avatar='https://randomuser.me/api/portraits/women/61.jpg'
-        online
-      />
-      <Contact
-        name='Albert Kuhn'
-        avatar='https://randomuser.me/api/portraits/men/82.jpg'
-      />
-      <Contact
-        name='Pamela Simpson'
-        avatar='https://randomuser.me/api/portraits/women/43.jpg'
-        online
-      />
+      <ContactList />
     </div>
   );
 }
