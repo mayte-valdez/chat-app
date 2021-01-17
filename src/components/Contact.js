a45cdbc351511fe272f18aff1e6d20b8f1cd630b
Mayte <mayte.valdez.abad@gmail.com>
2021-01-17 21:02:56 +0100

    add state & props - Quete React  05

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,19 +2,34 @@ import React from 'react';
 import './Contact.css'
 import PropTypes from 'prop-types';
 
-function Contact(props) {
-    return (
-        <div className='Contact'>
-            <img className='avatar' src={props.avatar} />
-            <div>
-                <h4 className='name'>{props.name}</h4>
-                <div className='status'>
-                    <div className={props.online ? 'status-online':'status-offline'}></div>
-                    <p className='status-text'>{props.online ? 'Online':'Offline' }</p>
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            online:this.props.online
+        };
+    }
+    
+    render(){
+        return (
+            <div className='Contact'>
+                <img className='avatar' src={this.props.avatar} />
+                <div>
+                    <h4 className='name'>{this.props.name}</h4>
+                    <div className='status'>
+                        <div onClick={this.newStatus} className={this.state.online ? 'status-online':'status-offline'}></div>
+                        <p onClick={ event => {
+                            const newStatus = !this.state.online;
+                            this.setState({online:newStatus});
+                            }} 
+                            className='status-text'
+                        >{this.state.online ? 'Online':'Offline' }</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+    
 }
 
 Contact.propTypes = {
